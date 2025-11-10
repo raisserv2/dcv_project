7e9f48bbb0d997425d1ac8cb55a3f033c3a6bdc5
MohdAseel <mohdaseelvkd@gmail.com>
2025-11-11 03:02:23 +0530

    Restore "Deck Archetypes" link in the navbar

--- #5 Data Visualization/app.py
+++ #5 Data Visualization/app.py
@@ -27,8 +27,8 @@ navbar = dbc.NavbarSimple(
         dbc.NavItem(dbc.NavLink("Builder", href="/builder")),
         dbc.NavItem(dbc.NavLink("Combos", href="/combined")),
         dbc.NavItem(dbc.NavLink("Evo", href="/evo")),
-        dbc.NavItem(dbc.NavLink("Deck Archetypes", href="/deck")),
         dbc.NavItem(dbc.NavLink("Rarity Analysis", href="/rarity"))
+        dbc.NavItem(dbc.NavLink("Deck Archetypes", href="/deck")),
     ],
 )
 
